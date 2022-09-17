f318fafa64c3ab6ab3ec72dcd980dd4de4fc7f61
LHD0617 <320388825@qq.com>
2022-09-17 16:29:07 +0800

    202209171628去掉样式表

--- RLCOM/main.cpp
+++ RLCOM/main.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C), 1988-1999, Xxxxxx Tech. Co., Ltd.
+ * FileName: main.cpp
+ * Description: main函数
+ * Change Logs:
+  |Date          |Author       |Notes     			|version
+  |2022-09-17    |满心欢喜     |Initial build     	|1.0.0
+ */
+
+/*  @include  */
 #include "MainWindow.h"
 #include <QSharedMemory>
 #include <QApplication>
@@ -9,7 +19,7 @@ int main(int argc, char *argv[])
 
     QSharedMemory shared_memory;
 
-    shared_memory.setKey(QString("9982"));
+    shared_memory.setKey(QString("RLCOM"));
 
     if(shared_memory.attach())
     {
@@ -23,20 +33,6 @@ int main(int argc, char *argv[])
     {
         MainWindow ui;
 
-        QFile file("flatgray.css");
-
-        if(file.open(QFile::ReadOnly))
-        {
-            QString qss = QLatin1String(file.readAll());
-
-            QString paletteColor = qss.mid(20, 7);
-
-            ui.setPalette(QPalette(QColor(paletteColor)));
-
-            ui.setStyleSheet(qss);
-
-            file.close();
-        }
         ui.show();
 
         return app.exec();
